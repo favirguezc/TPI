d50b7a736a2106f6a3354cbf89144908f3a4763b
favirguezc <favirguezc@gmail.com>
2014-06-02 18:02:25 -0500

    Test unit para labor cultural iniciada

--- TrazabilidadFresas/src/Main.java
+++ TrazabilidadFresas/src/Main.java
@@ -1,6 +1,8 @@
 
 import control.FresicultorControl;
+import control.LaborCulturalControl;
 import modelo.Fresicultor;
+import modelo.LaborCultural;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -21,8 +23,17 @@ public class Main {
 //        f.setCedula(1020794235);
 //        f.setFecha_de_nacimiento(new Date(1994, 7, 2));
 //        
-        Fresicultor f2 = new FresicultorControl().leer(1020794235);
-        System.out.println(f2.getNombres());
-        System.out.println(new FresicultorControl().leer(0));
+//        Fresicultor f2 = new FresicultorControl().leer(1020794235);
+//        System.out.println(f2.getNombres());
+//        System.out.println(new FresicultorControl().leer(0));
+        
+//        LaborCultural lc = new LaborCultural();
+//        lc.setNombre("Arrancar plantas");
+//        LaborCulturalControl lcc = new LaborCulturalControl();
+//        lcc.crear(lc);
+//        lc = new LaborCultural();
+//        lc.setNombre("Colocar plastico");
+//        lcc.crear(lc);
+//        System.out.println(lcc.leerTodos().size());
     }
 }
---
+++ TrazabilidadFresas/test/control/LaborCulturalControlTest.java
@@ -0,0 +1,113 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package control;
+
+import java.util.List;
+import modelo.LaborCultural;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author fredy
+ */
+public class LaborCulturalControlTest {
+    
+    public LaborCulturalControlTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of crear method, of class LaborCulturalControl.
+     */
+    @Test
+    public void testCrear() {
+        LaborCultural f = new LaborCultural();
+        f.setNombre("cre");
+        LaborCulturalControl instance = new LaborCulturalControl();
+        boolean expResult = false;
+        boolean result = instance.crear(f);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of eliminar method, of class LaborCulturalControl.
+     */
+    @Test
+    public void testEliminar() {
+        System.out.println("eliminar");
+        LaborCultural f = null;
+        LaborCulturalControl instance = new LaborCulturalControl();
+        boolean expResult = false;
+        boolean result = instance.eliminar(f);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of actualizar method, of class LaborCulturalControl.
+     */
+    @Test
+    public void testActualizar() {
+        System.out.println("actualizar");
+        LaborCultural f = null;
+        LaborCulturalControl instance = new LaborCulturalControl();
+        instance.actualizar(f);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of leer method, of class LaborCulturalControl.
+     */
+    @Test
+    public void testLeer() {
+        System.out.println("leer");
+        long id = 0L;
+        LaborCulturalControl instance = new LaborCulturalControl();
+        LaborCultural expResult = null;
+        LaborCultural result = instance.leer(id);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of leerTodos method, of class LaborCulturalControl.
+     */
+    @Test
+    public void testLeerTodos() {
+        System.out.println("leerTodos");
+        LaborCulturalControl instance = new LaborCulturalControl();
+        List<LaborCultural> expResult = null;
+        List<LaborCultural> result = instance.leerTodos();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
