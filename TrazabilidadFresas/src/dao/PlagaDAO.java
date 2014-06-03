ac771f2175b662fc0f9ecf0ed54b3d5e0508bcf8
favirguezc <favirguezc@gmail.com>
2014-06-02 20:02:45 -0500

    plagaDAO corregido

--- TrazabilidadFresas/src/dao/PlagaDAO.java
+++ TrazabilidadFresas/src/dao/PlagaDAO.java
@@ -47,8 +47,6 @@ public class PlagaDAO {
         Plaga r = null;
         try {
             r = (Plaga) em.createQuery("SELECT p FROM Plaga p WHERE p.id = :id").setParameter("id", id).getSingleResult();
-        } catch (NonUniqueResultException n) {
-            r = (Plaga) em.createQuery("SELECT p FROM Plaga p WHERE p.id = :id").setParameter("id", id).getResultList().get(0);
         } catch (Exception e) {
         } finally {
             em.close();
@@ -120,7 +118,6 @@ public class PlagaDAO {
             } catch (Exception e) {
             } finally {
                 em.close();
-                return ret;
             }
         }
         return ret;
