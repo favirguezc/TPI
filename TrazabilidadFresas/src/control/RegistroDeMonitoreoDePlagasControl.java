871c23138837a1647760aeabce7aad575344da94
favirguezc <favirguezc@gmail.com>
2014-06-02 20:28:41 -0500

    Registro de monitoreo de plagas arreglado

--- TrazabilidadFresas/src/control/RegistroDeMonitoreoDePlagasControl.java
+++ TrazabilidadFresas/src/control/RegistroDeMonitoreoDePlagasControl.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package control;
 
 import dao.RegistroDeMonitoreoDePlagasDAO;
@@ -15,37 +14,44 @@ import modelo.RegistroDeMonitoreoDePlagas;
  * @author JohnFredy
  */
 public class RegistroDeMonitoreoDePlagasControl {
-    
+
     RegistroDeMonitoreoDePlagasDAO dao = new RegistroDeMonitoreoDePlagasDAO();
-    
+
     public boolean crear(RegistroDeMonitoreoDePlagas r) {
         return validar(r) && dao.create(r);
     }
-    
+
     public boolean eliminar(RegistroDeMonitoreoDePlagas r) {
         return dao.delete(r.getId());
     }
-    
+
     public void actualizar(RegistroDeMonitoreoDePlagas r) {
         if (validar(r)) {
             dao.update(r);
         }
     }
-    
+
     public RegistroDeMonitoreoDePlagas leer(long id) {
         RegistroDeMonitoreoDePlagas r = dao.read(id);
         return r;
     }
+
     public List<RegistroDeMonitoreoDePlagas> leerTodos() {
         return dao.readAll();
     }
-    
-    private boolean validar(RegistroDeMonitoreoDePlagas r) {        
+
+    private boolean validar(RegistroDeMonitoreoDePlagas r) {
         if (r.getMonitoreoDePlagas() == null
-                || r.getValoracion()< 0     //No se si valorizacion puede ser menor que cero
-                || r.getId() < 0
-                || r.getPlaga() == null) {
+                || r.getValoracion() < 0 //No se si valorizacion puede ser menor que cero
+                || r.getPlaga() == null
+                || r.getValoracion() > r.getPlaga().getMaxima_valoracion()) {
             return false;
+        }
+        for (RegistroDeMonitoreoDePlagas rmp : leerTodos()) {
+            if (rmp.getPlaga().getId() == r.getPlaga().getId()
+                    && rmp.getMonitoreoDePlagas().getId() == r.getMonitoreoDePlagas().getId()) {
+                return false;
+            }
         }
         return true;
     }
